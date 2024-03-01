146d8f0e90daed3bebaa01e57dc66e67b7a72e64
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-03-01 19:57:41 -0300

    Atualizar função de desligamento do PC

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ import os
 import threading
 import time
 
-# Função para desligar o PC
 def desligar():
     try:
         tempo = int(entry.get())
-        tempo = tempo * 60 # Converter para minutos
+        tempo = tempo * 60
         if tempo < 0:
             messagebox.showerror('Erro', 'Digite um número positivo')
             return
         else:
             threading.Thread(target=desligar_pc, args=(tempo,)).start()
+            messagebox.showinfo('Sucesso', 'Timer de desligamento iniciado com sucesso!')
     except ValueError:
         messagebox.showerror('Erro', 'Digite um número válido')
         return
 
 def desligar_pc(tempo):
     try:
-        # Esperar o tempo especificado antes de desligar
         time.sleep(tempo)
-        os.system('shutdown /s /t 1')  # Desligar o PC
+        os.system('shutdown /s /t 1')
     except Exception as e:
         messagebox.showerror('Erro', f'Ocorreu um erro ao desligar o PC: {e}')
 
@@ -33,7 +32,7 @@ window.geometry('250x150')
 label = ttk.Label(window, text='Tempo para desligar (em minutos):')
 label.pack(pady=10)
 
-entry = ttk.Entry(window, width=15, font=('Arial', 14), justify='center')
+entry = ttk.Entry(window, width=15, font=('Arial', 12), justify='center')
 entry.pack()
 
 button = ttk.Button(window, text='Desligar', command=desligar)
